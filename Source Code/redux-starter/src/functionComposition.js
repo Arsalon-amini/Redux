97802eddecb4856ae3f257eb1ba7fd1105292485
arsalon <arsalonamini2020@gmail.com>
2020-10-13 13:30:49 +0800

    refactor functionCompositions  using lodash pipe and compose util fns

--- Source Code/redux-starter/src/functionComposition.js
+++ Source Code/redux-starter/src/functionComposition.js
@@ -1,3 +1,5 @@
+import { compose, pipe } from "lodash/fp"; //utility fns for functional programming
+
 //non-functional style of programming
 let input = "    Javascript   ";
 let output = "<div>" + input.trim() + "</div>";
@@ -8,3 +10,11 @@ const wrapInDiv = (str) => `<div> ${str} </div>`;
 const toLowerCase = (str) => str.toLowerCase();
 
 const result = wrapInDiv(toLowerCase(trim(input))); //fn composition
+
+//using lodash (compose)
+const transform = compose(wrapInDiv, toLowerCase, trim); //HoF -> takes fns -> returns new fn -> NOTE: passing fn references, not calling fn
+transform(input);
+
+//using lodash (pipe)
+const transform2 = pipe(trim, toLowerCase, wrapInDiv); //reads left to right (cleaner, more readable)
+transform(input);
