16cd83a20b746494f2e9a9a5077d649490e561ec
arsalon <arsalonamini2020@gmail.com>
2020-10-13 13:14:14 +0800

    higherOrderFunctions refresher

---
+++ Source Code/redux-starter/src/higherOrderFunctions.js
@@ -0,0 +1,4 @@
+let numbers = [1, 2, 3];
+numbers.map((number) => number * 2); //map is a higher order fn -> it takes a fn as an argument
+
+setTimeout(() => console.log("Hello"), 1000); //takes a fn as arg -> higher order fn
