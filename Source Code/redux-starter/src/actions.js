c1f2b5ce573c6d55706f615c45b0bf0951b03711
arsalon <arsalonamini2020@gmail.com>
2020-10-14 20:33:05 +0800

    Refactor: change action creators into arrow functions (cleaner syntax)

--- Source Code/redux-starter/src/actions.js
+++ Source Code/redux-starter/src/actions.js
@@ -1,20 +1,15 @@
 import * as actions from "./actionTypes";
 
+export const bugAdded = (description) => ({
+  type: actions.BUG_ADDED,
+  payload: {
+    description: description,
+  },
+});
 
-export function bugAdded(description) {
-  return {
-    type: actions.BUG_ADDED,
-    payload: {
-      description: description,
-    },
-  };
-}
-
-export function bugRemoved(id) {
-    return {
-      type: actions.BUG_REMOVED,
-      payload: {
-        id: id,
-      },
-    };
-  }
+export const bugRemoved = (id) => ({
+  type: actions.BUG_REMOVED,
+  payload: {
+    id: id,
+  },
+});
