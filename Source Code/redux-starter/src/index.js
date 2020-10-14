27c7867ab4cea56d8e2094dd363a37b8bdb9869a
arsalon <arsalonamini2020@gmail.com>
2020-10-14 19:49:48 +0800

    subscribing to the store

--- Source Code/redux-starter/src/index.js
+++ Source Code/redux-starter/src/index.js
@@ -1,17 +1,21 @@
 import store from "./store";
 
-store.dispatch({
-    type: "bugAdded",
-    payload: {
-        description: "Bug1"
-    }
-}); 
+store.subscribe(() => {
+  console.log("Store changed!", store.getState()); //called everytime state of store changes
+});
 
 store.dispatch({
-    type: "bugRemoved",
-    payload: {
-        id: 1
-    }
-})
+  type: "bugAdded",
+  payload: {
+    description: "Bug1",
+  },
+});
+
+store.dispatch({
+  type: "bugRemoved",
+  payload: {
+    id: 1,
+  },
+});
 
 console.log(store.getState());
