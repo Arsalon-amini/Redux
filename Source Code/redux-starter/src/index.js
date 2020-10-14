2c491c32090dbf4cdbe4182e9c4a9c4bb923fbd2
arsalon <arsalonamini2020@gmail.com>
2020-10-14 20:30:35 +0800

    Refactor: extract logic for creating an action into a separation module action.js and import in index.js when dispatching an action type

--- Source Code/redux-starter/src/index.js
+++ Source Code/redux-starter/src/index.js
@@ -1,22 +1,12 @@
 import store from "./store";
-import * as actions from "./actionTypes";
+import * as actions from './actions';
 
 const unsubscribe = store.subscribe(() => {
   console.log("Store changed!", store.getState()); //called everytime state of store changes
 });
 
-store.dispatch({
-  type: actions.BUG_ADDED,
-  payload: {
-    description: "Bug1",
-  },
-});
+store.dispatch(actions.bugAdded("Bug 1"));
 
-store.dispatch({
-  type: actions.BUG_REMOVED,
-  payload: {
-    id: 1,
-  },
-});
+store.dispatch(actions.bugRemoved(1));
 
 console.log(store.getState());
