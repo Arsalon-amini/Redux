da29460db124dfcec6179a39871fb52dbfe0979e
arsalon <arsalonamini2020@gmail.com>
2020-10-15 14:51:41 +0800

    Building Redux store from scratch - dispatch function

--- Source Code/redux-starter/src/index.js
+++ Source Code/redux-starter/src/index.js
@@ -1,4 +1,8 @@
 import store from './customStore';
-store.state = 1; 
-console.log(store); 
+import * as actions from './actions'; 
+
+store.dispatch(actions.bugAdded("Bug 1")); 
+
+
+console.log(store.getState()); 
 
