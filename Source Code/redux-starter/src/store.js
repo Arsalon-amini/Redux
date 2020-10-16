954df9690833408f37368dde182def6c0ca2257f
arsalon <arsalonamini2020@gmail.com>
2020-10-16 15:35:10 +0800

    adding Redux Dev Tools mapping / tracing

--- Source Code/redux-starter/src/store.js
+++ Source Code/redux-starter/src/store.js
@@ -1,26 +1,7 @@
 import { createStore } from "redux";
+import { devToolsEnhancer } from "redux-devtools-extension";
 import reducer from "./reducer";
 
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store = createStore(reducer, devToolsEnhancer({ trace: true }));
 
 export default store;
-
-//store object (simplified case) - array of bug objects
-// [
-//     {id: 2, description: 'bug description', resolved: false},
-//     {id: 3, description: 'bug description', resolved: false},
-//     {id: 4, description: 'bug description', resolved: false},
-// ]
-
-//store object (single JS object representing state) - real app
-// {
-//     bugs: [
-//         id: 1,
-//         description: "a bug description",
-//         resolved: false
-//     ],
-//     currentUser: { }
-// }
