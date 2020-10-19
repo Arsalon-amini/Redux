479502860675ad28a8f1c3b32ff016503b77473b
arsalon <arsalonamini2020@gmail.com>
2020-10-19 17:22:29 +0800

    Refactor Reducers and Actions: using createSlice () method from Redux toolkit

--- Source Code/redux-starter/src/store/bugs.js
+++ Source Code/redux-starter/src/store/bugs.js
@@ -1,27 +1,29 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
-
-//Action Creators
-
-export const bugAdded = createAction("bugAdded");
-export const bugResolved = createAction("bugResolved");
-export const bugRemoved = createAction("bugRemoved");
+import { createAction, createReducer, createSlice } from "@reduxjs/toolkit";
 
 //Reducers
+
 let lastId = 0;
 
-export default createReducer([], {
-  [bugAdded.type]: (bugs, action) => {
-    bugs.push({
-      id: ++lastId,
-      description: action.payload.description, //add new bug object (ID, description, resolved)
-      resolved: false,
-    });
-  },
-  [bugDeleted.type]: (bugs, action) => {
-    bugs.filter((bug) => bug.id !== action.payload.id);
-  },
-  [bugResolved.type]: (bugs, action) => {
-    const index = bugs.findIndex((bug) => bug.id === action.payload.id);
-    bugs[index].resolved = true;
+const slice = createSlice({
+  name: "bugs",
+  initialState: [],
+  reducers: {
+    bugAdded: (bugs, action) => {
+      bugs.push({
+        id: ++lastId,
+        description: action.payload.description, //add new bug object (ID, description, resolved)
+        resolved: false,
+      });
+    },
+    bugDeleted: (bugs, action) => {
+      bugs.filter((bug) => bug.id !== action.payload.id);
+    },
+    bugResolved: (bugs, action) => {
+      const index = bugs.findIndex((bug) => bug.id === action.payload.id);
+      bugs[index].resolved = true;
+    },
   },
 });
+
+export const { bugAdded, bugDeleted, bugResolved } = slice.actions;
+export default slice.reducer;
