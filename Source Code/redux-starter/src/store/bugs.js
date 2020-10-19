e3db466782fec3f9ffc731f9885ac925b63330cd
arsalon <arsalonamini2020@gmail.com>
2020-10-19 16:17:57 +0800

    Refactor Reducers: using redux toolkit and the createReducer () method - map object arguments (actionName, reducerFunction(state, actionType)

--- Source Code/redux-starter/src/store/bugs.js
+++ Source Code/redux-starter/src/store/bugs.js
@@ -6,24 +6,21 @@ export const bugAdded = createAction("bugAdded");
 export const bugResolved = createAction("bugResolved");
 export const bugRemoved = createAction("bugRemoved");
 
-//Reducer
+//Reducers
 let lastId = 0;
 
-//arg 1 - initial state, arg 2 - (key=actions, value=fns that handle actions), obj that maps objs to fns that handle action
-//maps actions: functions (events => event handlers)
-//fn passed takes two args (state, action) can rename to alias
 export default createReducer([], {
-  bugAddd: (bugs, action) => {
+  [bugAdded.type]: (bugs, action) => {
     bugs.push({
       id: ++lastId,
       description: action.payload.description, //add new bug object (ID, description, resolved)
       resolved: false,
     });
   },
-  bugDeleted: (bugs, action) => {
+  [bugDeleted.type]: (bugs, action) => {
     bugs.filter((bug) => bug.id !== action.payload.id);
   },
-  bugResolved: (bugs, action) => {
+  [bugResolved.type]: (bugs, action) => {
     const index = bugs.findIndex((bug) => bug.id === action.payload.id);
     bugs[index].resolved = true;
   },
