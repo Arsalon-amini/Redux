86354c49fab3737bf19b27b4152db4fe97133731
arsalon <arsalonamini2020@gmail.com>
2020-10-19 12:46:40 +0800

    Refactor: action creators refactored using Redux toolkit

--- Source Code/redux-starter/src/store/bugs.js
+++ Source Code/redux-starter/src/store/bugs.js
@@ -1,36 +1,17 @@
-//Action types
-const BUG_ADDED = "bugAdded";
-const BUG_REMOVED = "bugRemoved";
-const BUG_RESOLVED = "bugResolved";
+import { createAction } from "@reduxjs/toolkit";
 
 //Action Creators
-export const bugAdded = (description) => ({
-  type: BUG_ADDED,
-  payload: {
-    description,
-  },
-});
 
-export const bugRemoved = (id) => ({
-  type: BUG_REMOVED,
-  payload: {
-    id: id,
-  },
-});
-
-export const bugResolved = (id) => ({
-  type: BUG_RESOLVED,
-  payload: {
-    id,
-  },
-});
+export const bugAdded = createAction("bugAdded");
+export const bugResolved = createAction("bugResolved");
+export const bugRemoved = createAction("bugRemoved");
 
 //Reducer
 let lastId = 0;
 
 export default function reducer(state = [], action) {
   switch (action.type) {
-    case BUG_ADDED:
+    case bugAdded.type:
       return [
         ...state, //copy all bugs in current state
         {
@@ -39,9 +20,9 @@ export default function reducer(state = [], action) {
           resolved: false,
         },
       ];
-    case BUG_REMOVED:
+    case bugResolved.type:
       return state.filter((bug) => bug.id !== action.payload.id); //creates new array, without bug w/ given ID
-    case BUG_RESOLVED:
+    case bugResolved.type:
       return state.map((bug) =>
         bug.id !== action.payload.id ? bug : { ...bug, resolved: true }
       );
