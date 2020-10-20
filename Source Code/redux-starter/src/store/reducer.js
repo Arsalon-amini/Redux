a18ea014cbd0e6733de369521575a0024186d371
arsalon <arsalonamini2020@gmail.com>
2020-10-20 13:53:26 +0800

    Refactor: nest bugs and project slices under an entity parent

--- Source Code/redux-starter/src/store/reducer.js
+++ Source Code/redux-starter/src/store/reducer.js
@@ -1,8 +1,6 @@
-import {combineReducers} from "redux"; 
-import bugsReducer from './bugs';
-import projectsReducer from './projects';
+import { combineReducers } from 'redux';
+import entitiesReducer from './entities';
 
 export default combineReducers({
-    bugs: bugsReducer,
-    projects: projectsReducer
-}); 
+    entities: entitiesReducer
+})
