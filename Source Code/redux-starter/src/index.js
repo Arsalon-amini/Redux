661840987819f5b652997c36f3eaab019e0cfb40
arsalon <arsalonamini2020@gmail.com>
2020-10-20 16:59:24 +0800

    New Feature: dispatching functions using custom middleware to simlulate API calls

--- Source Code/redux-starter/src/index.js
+++ Source Code/redux-starter/src/index.js
@@ -5,27 +5,6 @@ import { projectAdded, projectRemoved } from "./store/projects";
 
 const store = configureStore();
 
-store.subscribe(() => {
-  console.log("store changed", store.getState());
-});
-
-store.dispatch(userAdded({ name: "arsalon", id: 1 }));
-// store.dispatch(userAdded({ name: "Melanie", id: 2 }));
-// store.dispatch(userAdded({ name: "Stratos", id: 3 }));
-
-
-
-// store.dispatch(bugAdded({ description: "Bug 1" }));
-// store.dispatch(bugAdded({ description: "Bug 2" }));
-// store.dispatch(bugAdded({ description: "Bug 3" }));
-// store.dispatch(bugResolved({ id: 2 }));
-
-// store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
-
-// store.dispatch(projectAdded({ description: "Project 1" }));
-// store.dispatch(projectAdded({ description: "Project 3" }));
-// store.dispatch(projectAdded({ description: "Project 4" }));
-// store.dispatch(projectRemoved({ id: 1 }));
-
-const bugs = getBugsByUser(1)(store.getState()); 
-console.log(bugs); 
+store.dispatch(() => {
+  store.dispatch({ type: 'bugsRecieved', bugs: [1 , 2, 3 ]}); //simulating API call response.data
+})
