3eb4c32ac55db6a0228df3a19fecce2bc066e70a
arsalon <arsalonamini2020@gmail.com>
2020-10-20 16:39:00 +0800

    New Feature: Middleware fn for logging

--- Source Code/redux-starter/src/index.js
+++ Source Code/redux-starter/src/index.js
@@ -10,22 +10,22 @@ store.subscribe(() => {
 });
 
 store.dispatch(userAdded({ name: "arsalon", id: 1 }));
-store.dispatch(userAdded({ name: "Melanie", id: 2 }));
-store.dispatch(userAdded({ name: "Stratos", id: 3 }));
+// store.dispatch(userAdded({ name: "Melanie", id: 2 }));
+// store.dispatch(userAdded({ name: "Stratos", id: 3 }));
 
 
 
-store.dispatch(bugAdded({ description: "Bug 1" }));
-store.dispatch(bugAdded({ description: "Bug 2" }));
-store.dispatch(bugAdded({ description: "Bug 3" }));
-store.dispatch(bugResolved({ id: 2 }));
+// store.dispatch(bugAdded({ description: "Bug 1" }));
+// store.dispatch(bugAdded({ description: "Bug 2" }));
+// store.dispatch(bugAdded({ description: "Bug 3" }));
+// store.dispatch(bugResolved({ id: 2 }));
 
-store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
+// store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
 
-store.dispatch(projectAdded({ description: "Project 1" }));
-store.dispatch(projectAdded({ description: "Project 3" }));
-store.dispatch(projectAdded({ description: "Project 4" }));
-store.dispatch(projectRemoved({ id: 1 }));
+// store.dispatch(projectAdded({ description: "Project 1" }));
+// store.dispatch(projectAdded({ description: "Project 3" }));
+// store.dispatch(projectAdded({ description: "Project 4" }));
+// store.dispatch(projectRemoved({ id: 1 }));
 
 const bugs = getBugsByUser(1)(store.getState()); 
 console.log(bugs); 
