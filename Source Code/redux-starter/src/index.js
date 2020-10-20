8a75825708290a9adf0257faba1dd11364357cf3
arsalon <arsalonamini2020@gmail.com>
2020-10-20 17:15:00 +0800

    Refactor: using Redux-tool kit thunk middleware for dispatching functions

--- Source Code/redux-starter/src/index.js
+++ Source Code/redux-starter/src/index.js
@@ -1,10 +1,15 @@
 import configureStore from "./store/configureStore";
-import { bugAdded, bugResolved, bugAssignedToUser, getBugsByUser} from "./store/bugs";
+import {
+  bugAdded,
+  bugResolved,
+  bugAssignedToUser,
+  getBugsByUser,
+} from "./store/bugs";
 import { userAdded } from "./store/users";
 import { projectAdded, projectRemoved } from "./store/projects";
 
 const store = configureStore();
 
-store.dispatch(() => {
-  store.dispatch({ type: 'bugsRecieved', bugs: [1 , 2, 3 ]}); //simulating API call response.data
-})
+store.dispatch((dispatch, getState) => {
+  dispatch({ type: "bugsRecieved", bugs: [1, 2, 3] }); //simulating API call response.data
+});
