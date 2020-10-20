215b06560c54ddc5cc6aad2d6146cd1f50e88a13
arsalon <arsalonamini2020@gmail.com>
2020-10-20 14:14:02 +0800

    New Feature: computing derived data using Redux store

--- Source Code/redux-starter/src/index.js
+++ Source Code/redux-starter/src/index.js
@@ -1,5 +1,6 @@
 import configureStore from "./store/configureStore";
 import * as actions from "./store/bugs";
+import * as projects from "./store/projects";
 
 const store = configureStore();
 
@@ -7,9 +8,18 @@ store.subscribe(() => {
   console.log("store changed", store.getState());
 });
 
+store.dispatch(projects.projectAdded({ description: "Project 1" }));
+store.dispatch(projects.projectAdded({ description: "Project 3" }));
+store.dispatch(projects.projectAdded({ description: "Project 4" }));
+store.dispatch(projects.projectRemoved({ id: 1 }));
+
 store.dispatch(actions.bugAdded({ description: "Bug 1" }));
 store.dispatch(actions.bugAdded({ description: "Bug 2" }));
 store.dispatch(actions.bugAdded({ description: "Bug 3" }));
-store.dispatch(actions.bugResolved({ id: 1 }));
+store.dispatch(actions.bugDeleted({ id: 1 }));
 
-console.log(store.getState());
+const unresolvedBugs = store
+  .getState()
+  .entities.bugs.filter((bug) => !bug.resolved);
+
+console.log(unresolvedBugs);
