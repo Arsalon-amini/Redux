166f35c47657b0a6b644893ddaf829986165575b
arsalon <arsalonamini2020@gmail.com>
2020-10-22 13:47:51 +0800

    Jest: first test on math.js fn to demonstrate Jest syntax

--- Source Code/redux-starter/src/math.spec.js
+++ Source Code/redux-starter/src/math.spec.js
@@ -1,1 +1,13 @@
-it("first test", () => {});
+import { isEven } from "./math";
+
+describe("isEven", () => {
+  it("should return true if given an even number", () => {
+    const result = isEven(2); //system or function "under test"
+    expect(result).toEqual(true); //matcher
+  });
+
+  it("should return false if given an odd number", () => {
+    const result = isEven(3); //system or function "under test"
+    expect(result).toEqual(false); //matcher
+  });
+});
